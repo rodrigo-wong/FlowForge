bb68a124d6ec1635d9af46093aef859a4c77de8f
wongmacr <wongmacr@gmail.com>
2026-02-26 16:49:39 -0500

    Added Order model and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Order\Status;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'customer_id',
+        'total',
+        'paid_at',
+        'shipped_at',
+        'delivered_at',
+        'cancelled_at',
+        'refunded_at',
+    ];
+
+    protected $casts = [
+        'paid_at' => 'datetime',
+        'shipped_at' => 'datetime',
+        'delivered_at' => 'datetime',
+        'cancelled_at' => 'datetime',
+        'refunded_at' => 'datetime',
+    ];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+}
