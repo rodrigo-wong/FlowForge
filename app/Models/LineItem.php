f4baa95fb50066f7a584ad496132efed8cc20b3c
wongmacr <wongmacr@gmail.com>
2026-03-06 13:25:08 -0500

    [Feature] Add Product and LineItem models and migrations

---
+++ app/Models/LineItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LineItem extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'quantity',
+        'discount',
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
